5051c62f1348643050eba292e3a26ceb20b0f19d
BaschtiSchrag <sebastianschrag@gmail.com>
2025-04-16 13:16:25 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,9 @@ const modeToggle = document.getElementById('modeToggle');
 modeToggle.addEventListener('click', () => {
   document.body.classList.toggle('dark');
 });
+<script>
+  const toggleButton = document.querySelector('.mode-toggle');
+  toggleButton.addEventListener('click', () => {
+    document.body.classList.toggle('dark');
+  });
+</script>
